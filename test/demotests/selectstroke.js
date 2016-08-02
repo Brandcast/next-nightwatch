b0247321036320e07eab25c68e88aeacfb39f848
Kelli Zehnder <kelli@brandcast.com>
2016-08-01 19:02:04 -0700

    verify border css is applied to element

--- test/demotests/selectstroke.js
+++ test/demotests/selectstroke.js
@@ -4,13 +4,14 @@ module.exports = {
     browser
       .url('http://localhost:8080/')
       .waitForElementVisible('body', 1000)
+      
       //opens site
       .verify.elementPresent('h5')
       .moveToElement('h5', 1, 1)
       .doubleClick()
 
       //selects column
-     .frame(0) //selects iframe - must call for 
+     .frame(0) //selects iframe - must call to select anything within iframe
       .waitForElementVisible('[data-id="44856863-ffe6-44d4-8787-e0dad3c93ecf"]', 1000)
       .click('span[data-id="44856863-ffe6-44d4-8787-e0dad3c93ecf"]')
       .frame(null) //closes iframe
@@ -20,11 +21,15 @@ module.exports = {
       .click('div.btn[title="Stroke"]')
       .verify.elementPresent('[id="border-all_2"]')
       .click('[id="border-all_2"]')
-      .pause(500)
-
+      
       //close stroke panel
       .click('.svg-container')
       .verify.elementNotPresent('.panel-body')
+
+      .frame(0) //selects iframe - must call to select anything within iframe
+      //.waitForElementVisible('[data-id="44856863-ffe6-44d4-8787-e0dad3c93ecf"]', 1000)
+      .assert.cssProperty('.bc-text', 'border-width', '1px')
+      .frame(null)
       .end();
     }
 };
