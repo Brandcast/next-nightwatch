730e2ee7a82484f718d64649f8d0041d37e25a4c
Kelli Zehnder <kelli@brandcast.com>
2016-08-01 19:11:40 -0700

    verifying css property: color is applied to bc-text

--- test/demotests/selectcolor.js
+++ test/demotests/selectcolor.js
@@ -12,24 +12,25 @@ module.exports = {
       .pause(500)
     //}
 
-
-    .frame(0) //selects iframe - must call for 
+    .frame(0) //selects iframe - must call to select anything within iframe
       .waitForElementVisible('[data-id="44856863-ffe6-44d4-8787-e0dad3c93ecf"]', 1000)
       .click('span[data-id="44856863-ffe6-44d4-8787-e0dad3c93ecf"]')
       .frame(null) //closes iframe
+    
     //select color
-    .verify.elementPresent('.spectrum-color-band')
-    .moveToElement('.spectrum-color-band', 75, 25)
-    .mouseButtonClick()
-    //.end();
-    //'close color panel' : function (browser) {
-    //browser
-     //.assert.elementPresent('.panel-body.dormant')
+      .verify.elementPresent('.spectrum-color-band')
+      .moveToElement('.spectrum-color-band', 75, 25)
+      .mouseButtonClick()
+
+    //closes color panel
       .click('.svg-container')
       .pause(500)
       .verify.elementNotPresent('.panel-body.dormant')
-
-      
+    
+    //verify color applied via css
+     .frame(0) //selects iframe - must call to select anything within iframe
+      .assert.cssProperty('.bc-text', 'color', 'rgba(134, 122, 191, 1)')
+      .frame(null)
       .end();
     }
 };
