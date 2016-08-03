a2570d41036b1d8e720b51925518eba5af4576bb
Kelli Zehnder <kelli@brandcast.com>
2016-08-03 16:49:05 -0700

    setting up reporting via nightwatch son file, pageoject creation for local test (not working)

--- page-objects/localhosttest.js
+++ page-objects/localhosttest.js
@@ -1,18 +1,9 @@
+
 module.exports = {
-  'step one' : function (browser) {
-    browser
-      .url('http://localhost:8080/')
-      .waitForElementVisible('body', 1000)
-      
-      //.setValue('input[type=text]', 'nightwatch')
-      // .findElement ('.map-item-container', 1000)
-  },
-  
-  //'step two' : function (browser) {
-    //browser
-     // .click('button[name=btnG]')
-      //.pause(1000)
-      //.assert.containsText('#main', 'Night Watch')
-     //.end(); 
-  //}
+  url: 'http://localhost:8080/',
+  elements: {
+    sitelist: { 
+      selector: 'body'
+    },
+  }
 };
--- test/demotests/localhosttest.js
+++ test/demotests/localhosttest.js
@@ -2,17 +2,7 @@ module.exports = {
   'step one' : function (browser) {
     browser
       .url('http://localhost:8080/')
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('@sitelist', 1000)
       .end();
-      //.setValue('input[type=text]', 'nightwatch')
-      // .findElement ('.map-item-container', 1000)
   },
-  
-  //'step two' : function (browser) {
-    //browser
-     // .click('button[name=btnG]')
-      //.pause(1000)
-      //.assert.containsText('#main', 'Night Watch')
-     //.end(); 
-  //}
 };
